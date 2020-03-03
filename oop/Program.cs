40ccbbaef854ffe7bfb183b9656eef58200ed665
ohhoneey <57149528+ohhoneey@users.noreply.github.com>
2020-03-03 21:43:04 +0200

    Add files via upload

---
+++ oop/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var str1 = new Mystring("jhjjftjgk");
+            var str2 = new Mystring("jhdfhvgk");
+            var str3 = new Mystring("jhhfghy");
+            Mytext text = new Mytext();
+            text.AddString(str2);
+            text.AddString(str1);
+            text.AddString(str3);
+            int y = str1.Counting('j');
+            int z = str2.Counting('j');
+            int e = str3.Counting('h');
+          
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+            Console.WriteLine(e);
+            Console.WriteLine(text.Allsymbols());
+
+            text.ReplaceString(2, str1);
+            text.RemoveIdentical();
+            text.Erase();
+
+            Console.ReadKey();
+        }
+        
+    }
+}
