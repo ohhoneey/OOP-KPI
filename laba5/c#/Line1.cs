a520d4d3704b6b446e084cf3dfec07aaf6455727
ohhoneey <57149528+ohhoneey@users.noreply.github.com>
2020-03-30 17:15:35 +0300

    Add files via upload

---
+++ laba5/c#/Line1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP5
+{
+    class Line1 : Line
+    {
+
+        public float getX(){ return x; }//метод отримання даних об'єкта
+        public float getY(){ return y; }//метод отримання даних об'єкта
+        public float getX1(){ return x1; }//метод отримання даних об'єкта
+        public float getY1(){ return y1; }//метод отримання даних об'єкта
+
+        public Line1(float x, float y, float x1, float y1) ://конструктор з параметрами
+            base(x, y, x1, y1)
+        {
+          
+        }
+
+        public override double Len() //методи збільшення довжини вдвічі
+        {
+                return 2 * base.Len();
+        }
+
+        public void Draw() //виведення координат на екран
+        {
+            Console.WriteLine("Line start:({0},{1}) Line end:({2},{3})", x, y, x1, y1);
+        }
+
+    }
+}
---
+++ laba5/c#/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Line one = new Line(2, 3, 4, 5); //створення нового об'єкта Line
+            double f = one.Len(); //присвоєння змінній значення довжини лінії
+            Console.WriteLine("Початкова довжина {0}", f);
+            Line1 two = new Line1(2, 3, 4, 5); //створення нового об'єкта Line1
+            double r = two.Len(); //присвоєння змінній значення довжини лінії
+            two.Draw(); //виведення координат на екран
+            Console.WriteLine("Довжина пiсля збiльшення в 2 рази {0}", r);
+
+            Console.ReadKey();
+
+        }
+    }
+}
