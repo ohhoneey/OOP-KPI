445d1b5c31c09c35be22acd139b21cf2bea6f83f
ohhoneey <57149528+ohhoneey@users.noreply.github.com>
2020-05-01 23:34:11 +0300

    Add files via upload

---
+++ Laba_6/Laba_6/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+
+namespace Laba_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Кабанец Виктория ИС -92");
+            var exp = new List<Expression>();
+            exp.Add(new Expression(1, 0, 2, 3)); //сттворення та додавання об'єкта 1
+            exp.Add(new Expression(1, -1, 2, 3)); //сттворення та додавання об'єкта 2
+
+            foreach (var line in exp)
+            {
+                try
+                {
+
+                    Console.WriteLine("Solution {0}", line.GetAnswer()); //виведення розрахунку виразу
+                    
+                }
+                catch (DivideByZeroException e) //якщо виникне помилка,то виведеться повідомлення
+                {
+                    Console.WriteLine(e.Message);
+                }
+                catch (ArithmeticException e) //якщо виникне помилка,то виведеться повідомлення
+                {
+                    Console.WriteLine(e.Message);
+                }
+                
+                Console.WriteLine("Parameters of the expression");
+                line.Show(); //виведення змінних на екран
+                Console.WriteLine("***********************************");
+
+
+            }
+            
+
+    
+
+            Console.ReadKey();
+            
+        }
+    }
+
+}
